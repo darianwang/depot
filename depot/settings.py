497dc5d91ebb8676fc49c820e2bfc71295c489e7
Darian Wang <darian.wangjun@gmail.com>
2012-05-07 21:50:39 +0800

    添加app：Django-groundwork，这个app可以为django加入scaffold支持。这次提交完成的工作有：1、引入django-groundwork，2、修改depot/setting.py,在INSTALLED-APP中添加

--- depot/settings.py
+++ depot/settings.py
@@ -126,6 +126,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'depotapp',
+    'django-groundwork',
 )
 
 # A sample logging configuration. The only tangible logging
