d5f0600c11162f1f14e92c14fa334f504e3b3a57
Darian Wang <darian.wangjun@gmail.com>
2012-05-07 21:29:38 +0800

    创建model并部署model

--- depot/settings.py
+++ depot/settings.py
@@ -11,10 +11,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'OPTION': {
+        'read_default_file': '/etc/my.cnf',
+        }
+        'NAME': 'depot',                      # Or path to database file if using sqlite3.
+        'USER': 'darian',                      # Not used with sqlite3.
+        'PASSWORD': 'buxingde',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -122,6 +125,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    depotapp,
 )
 
 # A sample logging configuration. The only tangible logging
--- depotapp/models.py
+++ depotapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length = 100)
+    description = models.TextField()
+    image_url = models.CharField(max_length = 200)
+    price = models.DecimalField(max_digits = 8, decimal_places = 2)
+    
